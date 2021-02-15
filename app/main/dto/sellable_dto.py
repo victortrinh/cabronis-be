c17147fa0f46ea7c68dcb82d2357d5b29f507b28
Victor <victor.trinh@outlook.com>
2021-02-15 17:06:02 -0500

    Fix member names in sellable_dto

--- app/main/dto/sellable_dto.py
+++ app/main/dto/sellable_dto.py
@@ -14,5 +14,5 @@ class PackDTO:
         'price': fields.Float(required=True, description='The price of the pack'),
         'category': fields.String(required=False, description='The category of the pack'),
         'team': fields.String(required=False, description='The team of the pack'),
-        'theme': fields.String(required=True, description='The theme of the pack', enum=SellableTheme.member_names_)
+        'theme': fields.String(required=True, description='The theme of the pack', enum=SellableTheme._member_names_)
     })
